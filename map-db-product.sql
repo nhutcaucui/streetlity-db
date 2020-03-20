9b15c8bf9cb0f510ff2e54eb69b674dd196b37f1
nvnamsss <nvnam.c@gmail.com>
2020-03-20 16:50:03 +0700

    added production version for map db

---
+++ map-db-product.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE map;
+
+USE map;
+
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS streets;
+DROP TABLE IF EXISTS nodes;
+DROP TABLE IF EXISTS services;
+DROP TABLE IF EXISTS toilet;
+DROP TABLE IF EXISTS fuel;
+DROP TABLE IF EXISTS atm;
+DROP TABLE IF EXISTS maintenance;
+
+
+CREATE TABLE streets (
+	id varchar(15) NOT NULL,
+    generalinfo varchar(255),
+    nodenum int,
+    nodes varchar(500),
+	cost float,
+    oneway BOOLEAN DEFAULT 0,
+	onewayfrom varchar(50),
+CONSTRAINT street_key
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE nodes (
+	id int(10) NOT NULL AUTO_INCREMENT,
+    lat float,
+    lon float,
+    streets varchar(500),
+    CONSTRAINT node_key
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE services(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    CONSTRAINT service_key
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE toilet(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    lat float,
+    lon float,
+    isworking boolean,
+    CONSTRAINT toilet_key
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE fuel(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    lat float,
+    lon float,
+    CONSTRAINT fuel_key
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE atm(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    lat float,
+    lon float,
+    CONSTRAINT atm_key
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE maintenance(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    lat float,
+    lon float,
+    iscallable boolean,
+    phone varchar(10),
+    CONSTRAINT maintenance_key
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE review(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    score int NOT NULL,
+    body varchar(255),
+    belono int(10),
+    CONSTRAINT review_key
+	PRIMARY KEY (id),
+    CONSTRAINT belong_key
+    FOREIGN KEY (belono) REFERENCES maintenance (id)
+);
