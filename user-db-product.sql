1c429cfe679e50072fbf7b90943a01be884350d6
nvnamsss <nvmam.c@gmail.com>
2020-04-24 12:02:28 +0700

    update user db

--- user-db-product.sql
+++ user-db-product.sql
@@ -1,29 +1,35 @@
 DROP DATABASE IF EXISTS user;
-
 CREATE DATABASE user;
-
 USE user;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_infos;
 DROP TABLE IF EXISTS tokens;
 
-CREATE TABLE users (
-	id varchar(15) NOT NULL,
+
+CREATE TABLE users
+(
+    id varchar(15) NOT NULL,
     username varchar(32),
     passwd varchar(32),
-    role int,
-    CONSTRAINT user_key
-    PRIMARY KEY (id)
+    acces_level int,
+    info_id varchar(15),
+    CONSTRAINT info_key FOREIGN KEY (info_id) REFERENCES user_infos(id),
+    CONSTRAINT user_key PRIMARY KEY (id)
+
 );
 
-CREATE TABLE user_info (
+CREATE TABLE user_infos
+(
     id varchar(15) NOT NULL,
     email varchar(64),
-    CONSTRAINT user_key
-    PRIMARY KEY (id)
+    CONSTRAINT info_key PRIMARY KEY (id)
 );
 
-CREATE TABLE tokens (
+CREATE TABLE tokens
+(
     id varchar(15) NOT NULL,
-    refresh_token varchar(255)
+    refresh_token varchar(255),
+    CONSTRAINT user_key FOREIGN KEY (id) REFERENCES users(id),
+    CONSTRAINT token_key PRIMARY KEY (id)
 );
